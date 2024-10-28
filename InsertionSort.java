076eace422270d257a8ac35495141ba5e63033e4
Evan Yang <evanjyang4@gmail.com>
2024-10-27 22:51:33 -0700

    bare bones java algo

---
+++ InsertionSort.java
@@ -0,0 +1,23 @@
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int theArray[] = {10, 2, 33, 1};
+        sortAndPrint(theArray);
+    }
+
+    static void sortAndPrint(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+            System.out.println(java.util.Arrays.toString(arr));
+        }
+    }
+}
